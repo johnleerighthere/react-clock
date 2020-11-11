4410cecc776ac234f42f7b8f7a019fa5350db692
johnleerighthere <johnleerighthere@gmail.com>
2020-11-11 15:31:17 +0800

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom";
 
 // import App from "./App";
 
-function tick(){
-
-const element = (<div>The date is:{new Date().toLocaleTimeString()}</div>);
-
-ReactDOM.render(element, document.getElementById("root"));
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
-setInterval(tick, 1000)
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
 
-
+setInterval(tick, 1000);
 
